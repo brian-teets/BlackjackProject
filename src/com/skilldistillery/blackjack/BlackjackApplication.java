623af216b3effb9b845861b38d98355d97dd94aa
Brian Teets <bwt17@msn.com>
2022-04-10 17:25:52 -0600

    Nested my conditional game logic inside a loop to allow player to replay if desired.

--- src/com/skilldistillery/blackjack/BlackjackApplication.java
+++ src/com/skilldistillery/blackjack/BlackjackApplication.java
@@ -33,88 +33,102 @@ public class BlackjackApplication {
 	
 
 	public void gameSimulator() {
-		Scanner scanner = new Scanner(System.in);
-		Dealer dealer = new Dealer();
-		Player player = new Player();
-		System.out.println("The dealer will deal you 2 cards to begin the game.");
-		System.out.println("Dealing now... ");
-		System.out.println();
-		dealer.getDeck();
-		dealer.shuffleDeck();
-		dealer.dealNewHandToPlayer(player);
-		System.out.println();
-		System.out.println("Now the dealer will deal to self.");
-		dealer.dealNewHandToDealer(dealer);
-		System.out.println();
-
-		boolean keepGoing = true;
-		
-		while (keepGoing) {
+		String keepPlaying = "Y"; 
+		while( ! keepPlaying.equalsIgnoreCase("N")) {
+			Scanner scanner = new Scanner(System.in);
 			
-			if (player.getPlayerHandValue() == 21 || dealer.getDealerHandValue() == 21) {
-				int winnerValue = 0;
-				if (player.getPlayerHandValue() > dealer.getDealerHandValue()) {
-					winnerValue = player.getPlayerHandValue();
-					System.out.println("Player1 won with " + winnerValue);
-					System.out.println("Restart the game to play again.");
-					keepGoing = false;
-				} else if (dealer.getDealerHandValue() > player.getPlayerHandValue()) {
-					winnerValue = dealer.getDealerHandValue();
-					System.out.println("Dealer won with " + winnerValue);
-					System.out.println("Restart the game to play again.");
-					keepGoing = false;
-				}
-			} 
-			else if (player.getPlayerHandValue() < 21 || dealer.getDealerHandValue() < 21) {
-				System.out.println("Player1 has " + player.getPlayerHandValue()
-						+ ". Player1, do you want to hit or stay? " + " Enter either (Hit, or Stay)  ");
-				System.out.println();
-				String userInput = scanner.nextLine();
-				if (userInput.equalsIgnoreCase("hit")) {
-					dealer.dealACard(player);
-					System.out.println("Player1's new hand value is " + player.getPlayerHandValue());
-					if (player.getPlayerHand().isBust() == true) {
-						System.out.println("Sorry Player, you busted!");
+			Dealer dealer = new Dealer();
+			Player player = new Player();
+			System.out.println("The dealer will deal you 2 cards to begin the game.");
+			System.out.println("Dealing now... ");
+			System.out.println();
+			dealer.getDeck();
+			dealer.shuffleDeck();
+			dealer.dealNewHandToPlayer(player);
+			System.out.println();
+			System.out.println("Now the dealer will deal to self.");
+			dealer.dealNewHandToDealer(dealer);
+			System.out.println();
+
+			boolean keepGoing = true;
+			
+			while (keepGoing) {
+				
+				if (player.getPlayerHandValue() == 21 || dealer.getDealerHandValue() == 21) {
+					int winnerValue = 0;
+					if (player.getPlayerHandValue() > dealer.getDealerHandValue()) {
+						winnerValue = player.getPlayerHandValue();
+						System.out.println("Player1 won with " + winnerValue);
+						System.out.println("Restart the game to play again.");
+						keepGoing = false;
+					} else if (dealer.getDealerHandValue() > player.getPlayerHandValue()) {
+						winnerValue = dealer.getDealerHandValue();
+						System.out.println("Dealer won with " + winnerValue);
 						System.out.println("Restart the game to play again.");
 						keepGoing = false;
 					}
-					System.out.println();
-				}
-				if (userInput.equalsIgnoreCase("stay")) {
-					System.out.println("Player1 has chosen to stay. Player's current hand value is "
-							+ player.getPlayerHandValue());
-					System.out.println("Dealer's current hand value is " + dealer.getDealerHandValue());
-					System.out.println();
-					
-				}
-				if (dealer.getDealerHandValue() < 17) {
-					System.out.println();
-					System.out
-							.println("Dealer's hand value is " + dealer.getDealerHandValue() + ", so dealer must hit.");
-					dealer.dealACard(dealer);
-					System.out.println("Dealer's new value is " + dealer.getDealerHandValue());
-					if (dealer.getDealerHand().isBust() == true) {
-						System.out.println("Dealer busted. You win.");
-						System.out.println("Restart the game to play again.");
-						keepGoing = false;
-					} 
-
 				} 
-				else if (dealer.getDealerHandValue() >= 17) {
+				else if (player.getPlayerHandValue() < 21 || dealer.getDealerHandValue() < 21) {
+					System.out.println("Player1 has " + player.getPlayerHandValue()
+							+ ". Player1, do you want to hit or stay? " + " Enter either (Hit, or Stay)  ");
 					System.out.println();
-					System.out.println("Dealer's current hand value is " + dealer.getDealerHandValue());
-					if (dealer.getDealerHand().isBust() == true) {
-						System.out.println("Dealer busted. You win.");
-						System.out.println("Restart the game to play again.");
-						keepGoing = false;
+					String userInput = scanner.nextLine();
+					if (userInput.equalsIgnoreCase("hit")) {
+						dealer.dealACard(player);
+						System.out.println("Player1's new hand value is " + player.getPlayerHandValue());
+						if (player.getPlayerHand().isBust() == true) {
+							System.out.println("Sorry Player, you busted!");
+							System.out.println("Restart the game to play again.");
+							keepGoing = false;
+						}
+						System.out.println();
 					}
-					System.out.println();
+					if (userInput.equalsIgnoreCase("stay")) {
+						System.out.println("Player1 has chosen to stay. Player's current hand value is "
+								+ player.getPlayerHandValue());
+						System.out.println("Dealer's current hand value is " + dealer.getDealerHandValue());
+						
+					}
+					if (dealer.getDealerHandValue() < 17) {
+						System.out.println();
+						System.out
+								.println("Dealer's hand value is " + dealer.getDealerHandValue() + ", so dealer must hit.");
+						dealer.dealACard(dealer);
+						System.out.println("Dealer's new value is " + dealer.getDealerHandValue());
+						if (dealer.getDealerHand().isBust() == true) {
+							System.out.println("Dealer busted. You win.");
+							System.out.println("Restart the game to play again.");
+							keepGoing = false;
+						} 
+
+					} 
+					else if (dealer.getDealerHandValue() >= 17) {
+						System.out.println();
+						System.out.println("Dealer's current hand value is " + dealer.getDealerHandValue());
+						if (dealer.getDealerHand().isBust() == true) {
+							System.out.println("Dealer busted. You win.");
+							System.out.println("Restart the game to play again.");
+							keepGoing = false;
+						}
+						System.out.println();
+					}
+
 				}
+			
 
+			} // end of while loop
+			
+			System.out.println("Do you want to play again?  (Y / N)   ");
+			keepPlaying = scanner.nextLine();
+			if(keepPlaying.equalsIgnoreCase("N")) {
+				System.out.println("==============Goodbye!===============");
+				keepPlaying = "N";
 			}
-//		scanner.close();
-
-		} // end of while loop
+			else if(keepPlaying.equalsIgnoreCase("Y")) {
+				keepPlaying = "Y";
+			}
+			scanner.close();
+		}
 
 	} // end of gameSimulator method
 	
